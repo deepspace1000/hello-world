dc5947d2b9bc4a016a37f89e3f4e32a53db078fc
A1eeee <alejandro.wenzler@gmail.com>
2023-06-07 09:48:28 +0200

    feature B - new Attribut Name and getter and setter

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,15 @@
 public class HelloWorld {
     private int i;
+	private String name;
     public HelloWorld() {
         i = 0; }
     public int getI() {
         return i;
     } 
+	public String getName(){
+		return name;
+	}
+	public void setName(String n){
+		name = n;
+	}
 }
