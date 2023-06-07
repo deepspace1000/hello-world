0f3ee7e6620829f97d0a262057aa3a7ce9be8191
deepspace1000 <nilsrothe05@icloud.com>
2023-06-07 09:47:22 +0200

    feature A - new constructor

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 public class HelloWorld {
     private int i;
     public HelloWorld() {
-        i = 0; }
+        i = 0; 
+    }
+    public HelloWorld(String text){
+        System.out.println(text);
+    }
     public int getI() {
         return i;
     } 
